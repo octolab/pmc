1de65ec99a58443131d3e3e3fff86913f3997360
Kamil Samigullin <kamil@samigullin.info>
2016-06-05 23:50:02 +0300

    fix issue #5

---
+++ pmc/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestNotImplemented(unittest.TestCase):
+    def test_not_implemented(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
